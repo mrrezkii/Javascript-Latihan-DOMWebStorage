9498053b808becd9b025385b394c0cb84e7c7bb1
nando <muhammad.rezki.ananda@gmail.com>
2021-07-09 10:37:50 +0700

    Add data js

---
+++ data.js
@@ -0,0 +1,78 @@
+const STORAGE_KEY = "BOOKSHELF_APPS"
+
+/*
+{
+    id: string | number,
+    title: string,
+    author: string,
+    year: number,
+    isComplete: boolean,
+  }
+*/
+
+let bookshelf = []
+
+
+function isStorageExist() {
+    if(typeof(Storage) === undefined){
+        alert("Browser kamu tidak mendukung local storage")
+        return false
+    } 
+    return true;
+}
+
+function saveData(){
+    const parsed = JSON.stringify(bookshelf)
+    localStorage.setItem(STORAGE_KEY, parsed)
+    document.dispatchEvent(new Event("ondatasaved"))
+}
+
+function loadDataFromStorage() {
+    const serializedData = localStorage.getItem(STORAGE_KEY);
+    
+    let data = JSON.parse(serializedData)
+    
+    if(data !== null)
+        bookshelf = data
+
+    document.dispatchEvent(new Event("ondataloaded"))
+}
+
+function updateDataToStorage() {
+    if(isStorageExist())
+        saveData()
+}
+
+
+function composeTodoObject(title, author, year, isCompleted) {
+    return {
+        id: +new Date(),
+        title,
+        author,
+        year, 
+        isCompleted
+    };
+}
+
+function findBook(id){
+    for (book of bookshelf){
+        if(book.id === id){
+            return book
+        }
+    }
+
+    return null
+}
+
+function findBookIndex(id){
+    let index = 0 
+    for (book of bookshelf){
+        if(book.id === id){
+            return index
+        }
+
+        index++
+    }
+
+    return -1
+}
